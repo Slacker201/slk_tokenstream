fb3b8eba0aba9f16ddaf6f75f5c4c59732d8a923
Slacker201 <slacker1234567809@gmail.com>
2026-05-14 09:42:49 -0600

    Docs: add documentation and examples for Mark struct
    Added doc comments and usage examples for the `Mark` struct and its `position` method in `src/bookmark.rs`.

--- src/bookmark.rs
+++ src/bookmark.rs
@@ -1,3 +1,16 @@
+/// A mark struct used to mark positions in a `TokenStream` for backtracking    
+/// 
+/// # Examples
+/// ``` rust
+/// use tokenstream::tokenstream::TokenStream;
+/// let tokens = vec![1, 2, 3];
+/// let mut token_stream = TokenStream::new(tokens);
+/// let mark = token_stream.mark();
+/// token_stream.advance(5);
+/// assert_eq!(token_stream.peek(), None);
+/// token_stream.reset(&mark);
+/// assert_eq!(token_stream.peek(), Some(&1));
+/// ```
 #[derive(Debug)]
 pub struct Mark {
     position: usize,
@@ -9,6 +22,17 @@ impl Mark {
         Self { position }
     }
     /// Returns the position
+    /// 
+    /// # Example
+    /// 
+    /// ```
+    /// use tokenstream::bookmark::Mark;
+    /// use tokenstream::tokenstream::TokenStream;
+    /// 
+    /// let mut token_stream = TokenStream::new(vec![0; 12]);
+    /// token_stream.advance(12);
+    /// assert_eq!(token_stream.mark().position(), 12);
+    /// ```
     pub fn position(&self) -> usize {
         self.position
     }
